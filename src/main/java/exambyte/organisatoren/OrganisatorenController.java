df3e484d68a496eaa832e0b8a5da1934200114d7
Narr1M <miran.alci29@gmail.com>
2024-10-30 21:51:17 +0100

    addFreitext- und MCFragen Page hinzugefügt
    Methoden hinzugefügt, die ein Post-Request auslösen, wenn man eine MC- oder Freitextfrage hinzufügen will. Man wird zurück zur Test-Erstellen-Seite redirected und man kann eine Freitext- oder MC-Frage erstellen.

--- src/main/java/exambyte/organisatoren/OrganisatorenController.java
+++ src/main/java/exambyte/organisatoren/OrganisatorenController.java
@@ -1,22 +1,42 @@
 package exambyte.organisatoren;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class OrganisatorenController {
+
     @GetMapping("/organisatoren/landingPage")
     public String landingPage() {
         return "/organisatoren/LandingPageOrganisatoren";
     }
+
 
     @GetMapping("/organisatoren/testErstellen")
     public String testErstellen() {
         return "/organisatoren/TestErstellenPageOrganisatoren";
     }
 
+    @PostMapping("/organisatoren/testErstellen/addMcFrage")
+    public String addMcFrage(RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("mcFrageButton", true);
+
+        return "redirect:/organisatoren/testErstellen";
+    }
+
+    @PostMapping("/organisatoren/testErstellen/addFreitextFrage")
+    public String addFreitextFrage(RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("freitextFrageButton", true);
+
+        return "redirect:/organisatoren/testErstellen";
+    }
+
     @GetMapping("/organisatoren/korrekturstand")
     public String korrekturStand() {
+
         return "/organisatoren/KorrekturstandOrganisatoren";
     }
 }
