4d992b5a0e9dda38438053c5374d1c99ca76f710
Kxrstin <kerstinneu.damme@gmail.com>
2024-10-30 18:41:03 +0100

    Controller für Studenten und Tests
    Der Controller für Studenten wurde testgetrieben erstellt und die Ordnerstruktur wurde leicht angepasst.

---
+++ src/test/java/exambyte/studenten/StudentenControllerTest.java
@@ -0,0 +1,28 @@
+package exambyte.studenten;
+
+import exambyte.studenten.StudentenController;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(StudentenController.class)
+public class StudentenControllerTest {
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    @DisplayName("Die Route /landingPage führt zum Öffnen der LandingPageStudenten.html Seite und es gibt einen 200 Status.")
+    public void test_landingPage() throws Exception {
+        mvc.perform(get("/studenten/landingPage"))
+                .andExpect(view().name("studenten/LandingPageStudenten"))
+                .andExpect(status().isOk());
+          }
+
+}
