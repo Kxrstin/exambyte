912e31f6ac7e0940d2a2babcef2eda54031bf2ea
Kxrstin <147599239+Kxrstin@users.noreply.github.com>
2024-10-30 17:43:02 +0100

    Initial commit
    Hinzufügen der vorab grob vorbereiteten Dateien.

---
+++ src/main/java/exambyte/ExambyteProjektApplication.java
@@ -0,0 +1,13 @@
+package exambyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExambyteProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExambyteProjektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/exambyteprojekt/ExambyteProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.exambyteprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExambyteProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
