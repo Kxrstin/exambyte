e8866e26dd27da9e84a9b4c91a8b013356c15aae
Kxrstin <kerstinneu.damme@gmail.com>
2024-11-14 00:06:06 +0100

    Ungenutzte Import Statements löschen

--- src/main/java/exambyte/korrektoren/KorrektorenControllerLandingPage.java
+++ src/main/java/exambyte/korrektoren/KorrektorenControllerLandingPage.java
@@ -1,11 +1,8 @@
 package exambyte.korrektoren;
 
-import exambyte.studenten.TestStudenten;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.ArrayList;
 
 @Controller
 public class KorrektorenControllerLandingPage {
--- src/main/java/exambyte/studenten/StudentenControllerLandingPage.java
+++ src/main/java/exambyte/studenten/StudentenControllerLandingPage.java
@@ -6,9 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class StudentenControllerLandingPage {
