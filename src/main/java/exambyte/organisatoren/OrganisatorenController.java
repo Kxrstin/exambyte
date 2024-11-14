3321666de072eadccfb427e0defa5d93c8537d6b
Narr1M <miran.alci29@gmail.com>
2024-11-14 19:58:18 +0100

    Update Organisatoren Dateien
    Organisatoren Controller wurden mehr URLs hinzugefügt. Mehr Tests wurden für den Controller hinzugefügt. Organisatoren TestErstellenPageOrganisatoren wurde verschönert.

--- src/main/java/exambyte/organisatoren/OrganisatorenController.java
+++ src/main/java/exambyte/organisatoren/OrganisatorenController.java
@@ -1,5 +1,6 @@
 package exambyte.organisatoren;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,24 +10,46 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class OrganisatorenController {
 
     @GetMapping("/organisatoren/landingPage")
+    @Secured("ROLE_ORGANISATOR")
     public String landingPage() {
         return "/organisatoren/LandingPageOrganisatoren";
     }
 
 
     @GetMapping("/organisatoren/testErstellen")
+    @Secured("ROLE_ORGANISATOR")
     public String testErstellen() {
         return "/organisatoren/TestErstellenPageOrganisatoren";
     }
 
     @PostMapping("/organisatoren/testErstellen/addMcFrage")
+    @Secured("ROLE_ORGANISATOR")
     public String addMcFrage(RedirectAttributes redirectAttributes) {
         redirectAttributes.addFlashAttribute("mcFrageButton", true);
 
         return "redirect:/organisatoren/testErstellen";
     }
 
+    @PostMapping("/organisatoren/testErstellen/updateTitelMC")
+    @Secured("ROLE_ORGANISATOR")
+    public String updateTitelMC() {
+        return "redirect:/organisatoren/testErstellen";
+    }
+
+    @PostMapping("/organisatoren/testErstellen/updateFragestellungMC")
+    @Secured("ROLE_ORGANISATOR")
+    public String updateFragestellungMC() {
+        return "redirect:/organisatoren/testErstellen";
+    }
+
+    @PostMapping("/organisatoren/testErstellen/updatePunkteMC")
+    @Secured("ROLE_ORGANISATOR")
+    public String updatePunkteMC() {
+        return "redirect:/organisatoren/testErstellen";
+    }
+
     @PostMapping("/organisatoren/testErstellen/addFreitextFrage")
+    @Secured("ROLE_ORGANISATOR")
     public String addFreitextFrage(RedirectAttributes redirectAttributes) {
         redirectAttributes.addFlashAttribute("freitextFrageButton", true);
 
@@ -34,8 +57,8 @@ public class OrganisatorenController {
     }
 
     @GetMapping("/organisatoren/korrekturstand")
+    @Secured("ROLE_ORGANISATOR")
     public String korrekturStand() {
-
         return "/organisatoren/KorrekturstandOrganisatoren";
     }
 }
