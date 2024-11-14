a360eb416a67fe8962baab800f362ef63a38de8c
Kxrstin <kerstinneu.damme@gmail.com>
2024-11-14 01:06:50 +0100

    Anmeldeseite um Button erweitert und verschönert
    Die Anmeldeseite führt jetzt zu Exambyte. Dort muss noch die Logik ergänzt werden. Der User wird auf der Anmeldeseite begrüßt.

--- src/main/java/exambyte/anmeldung/AnmeldungController.java
+++ src/main/java/exambyte/anmeldung/AnmeldungController.java
@@ -1,5 +1,7 @@
 package exambyte.anmeldung;
 
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AnmeldungController {
     @GetMapping("/")
-    public String anmeldung() {
+    public String anmeldung(OAuth2AuthenticationToken auth, Model model) {
+        OAuth2User user = auth.getPrincipal();
+        String login = user.getAttribute("login");
+        model.addAttribute("login", login);
         return "anmeldung/AnmeldungPage";
     }
 
