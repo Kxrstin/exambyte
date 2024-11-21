51c8a93eae9401ddbcfbfc82c11365dd7ad10671
Kxrstin <kerstinneu.damme@gmail.com>
2024-11-21 14:45:40 +0100

    ArchTest hinzugefügt

---
+++ src/test/java/exambyte/ArchRuleTests.java
@@ -0,0 +1,31 @@
+package exambyte;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.assertj.ApplicationContextAssert;
+import org.springframework.stereotype.Controller;
+
+import java.lang.annotation.Annotation;
+import exambyte.ExambyteProjektApplication;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.library.Architectures.onionArchitecture;
+
+@AnalyzeClasses(packagesOf = ExambyteProjektApplication.class)
+public class ArchRuleTests {
+    @ArchTest
+    private final ArchRule nurControllerInControllerKlassen = classes()
+            .that()
+            .resideInAPackage("..controller..")
+            .and()
+            .haveSimpleNameContaining("Controller")
+            .should()
+            .beAnnotatedWith(Controller.class)
+            .orShould()
+            .beAnnotatedWith(WebMvcTest.class);
+}
