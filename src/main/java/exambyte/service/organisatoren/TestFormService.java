0a4e82f9d96e632f204bdcabe6e8ac28b148d564
miran <miran.alci29@gmail.com>
2024-12-15 11:54:53 +0100

    ADD TestFormService Zwischenspeicher und DB-Methoden
    TestFormService arbeitet nun mit einem Zwischenspeicher, der alle Änderungen beim Testerstellen für sich speichert aber nicht in die Datenbank speichert. Dementsprechend hat TestFormService jetzt auch Methoden, die Testformulare in der Datenbank speichern oder welche aus der Datenbank holen können.

--- src/main/java/exambyte/service/organisatoren/TestFormService.java
+++ src/main/java/exambyte/service/organisatoren/TestFormService.java
@@ -1,32 +1,52 @@
 package exambyte.service.organisatoren;
 
 import exambyte.aggregates.organisatoren.TestFormular;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class TestFormService {
     private TestFormRepo repository;
-
+    private Map<Integer, TestFormular> zwischenspeicher = new HashMap<>();
     public TestFormService(TestFormRepo repository) {
         this.repository = repository;
     }
 
     public TestFormular getTestFormById(Integer id) {
-        return repository.loadTestForm(id);
+        return zwischenspeicher.get(id);
     }
 
     public int addNewTestForm() {
-        return repository.addNewTestForm();
+        TestFormular testForm = new TestFormular("", new HashMap<>());
+        zwischenspeicher.put(testForm.getId(), testForm);
+        return testForm.getId();
     }
 
     public int saveTestForm(TestFormular testForm) {
-        return repository.saveTestForm(testForm);
+        int id = testForm.getId();
+        zwischenspeicher.put(id, testForm);
+        System.out.println(testForm.getTestTitel());
+        return id;
     }
 
     public List<TestFormular> getTestForms() {
+        return zwischenspeicher.entrySet().stream()
+                .map(entry -> entry.getValue())
+                .toList();
+    }
+
+    public TestFormular getTestFormByIdDB(Integer id) {
+        return repository.loadTestForm(id);
+    }
+
+    public int saveTestFormDB(TestFormular testForm) {
+        return repository.saveTestForm(testForm);
+    }
+
+    public List<TestFormular> getTestFormsDB() {
         return repository.getTestForms();
     }
 }
