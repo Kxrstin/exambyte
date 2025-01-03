90a53a8ca6973e12959afd3507360d00069e5e50
Narr1M <miran.alci29@gmail.com>
2025-01-03 13:09:09 +0100

    ADD Validation bei Testerstellen-Page (WIP)
    (WIP) Tests sollen keine leeren Felder haben, keine MC-Fragen ohne Antwortmöglichkeiten und eine Punktzahl haben, die größer als 0 ist.

--- src/main/java/exambyte/aggregates/organisatoren/FreitextFrage.java
+++ src/main/java/exambyte/aggregates/organisatoren/FreitextFrage.java
@@ -1,18 +1,20 @@
 package exambyte.aggregates.organisatoren;
 
 import exambyte.annotations.Wertobjekt;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 @Wertobjekt
 class FreitextFrage implements TestFrage {
-    private final Integer punkte;
-    private final String titel;
-    private final String fragestellung;
-    private final String beschreibung;
+    @Min(value = 1, message = "Darf nicht negativ sein") private final Integer punkte;
+    @NotBlank(message = "Darf nicht leer sein") private final String titel;
+    @NotBlank(message = "Darf nicht leer sein") private final String fragestellung;
+    @NotBlank(message = "Darf nicht leer sein") private final String beschreibung;
     private final Integer id;
-    private final String erklaerung;
+    @NotBlank(message = "Darf nicht leer sein") private final String erklaerung;
 
     public FreitextFrage(Integer punkte, String titel, String fragestellung, String beschreibung, String erklaerung) {
         this.punkte = punkte;
