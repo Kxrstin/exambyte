561bb83fcf2b94b024ab0cb6ab7084df754b3e1c
Kxrstin <kerstinneu.damme@gmail.com>
2025-01-04 22:33:41 +0100

    DELETE Ungenutzter Import

--- src/main/java/exambyte/aggregates/studenten/StudiAntwort/McAntwort.java
+++ src/main/java/exambyte/aggregates/studenten/StudiAntwort/McAntwort.java
@@ -4,7 +4,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.PersistenceCreator;
 import org.springframework.data.annotation.Transient;
 
-import javax.persistence.Column;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
